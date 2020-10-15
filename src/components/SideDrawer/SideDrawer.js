c34b68256bbc504ad45a534d147563ac1b4c2fb7
GURDAYAL SINGH DHILLON <20343529@students.latrobe.edu.au>
2020-10-15 11:55:07 +1100

    nav bar update

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SideDrawer.css";
 const SideDrawer = (props) => {
   let drawerClasses = ["side-drawer"];
@@ -10,10 +11,49 @@ const SideDrawer = (props) => {
     <nav className={drawerClasses.join(" ")}>
       <ul>
         <li>
-          <a href="/">Products</a>
+          <Link to="/login">
+            <i className="fas fa-sign-in-alt login-icon"></i>
+          </Link>
         </li>
         <li>
-          <a href="/">Users</a>
+          <Link to="/overflow">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About US</Link>
+        </li>
+        <li>
+          <Link to="/blogs">Blogs</Link>
+        </li>
+        <li>
+          <button className="button-contact"> Contact</button>
+        </li>
+
+        <li>
+          <Link to="/">Appointment Setting</Link>
+        </li>
+        <li>
+          <Link to="/">Telemarketing</Link>
+        </li>
+        <li>
+          <Link to="/">Data Cleaning</Link>
+        </li>
+        <li>
+          <Link to="/">Digital Lead Services</Link>
+        </li>
+        <li>
+          <Link to="/">Mobile & Web apps</Link>
+        </li>
+        <li>
+          <Link to="/">Reataurant-App</Link>
+        </li>
+        <li>
+          <Link to="/">Business Events</Link>
+        </li>
+        <li>
+          <Link to="/">Sales Training</Link>
+        </li>
+        <li>
+          <Link to="/">Marketing Automation</Link>
         </li>
       </ul>
     </nav>
