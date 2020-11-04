d54acc76e52004525f06a881b482319982f9cdd6
GURDAYAL SINGH DHILLON <20343529@students.latrobe.edu.au>
2020-11-04 15:19:46 +1100

    quiz results updated

--- src/Pages/QuizResults.js
+++ src/Pages/QuizResults.js
@@ -9,28 +9,28 @@ import ServiceOptions from "../components/serviceOptions";
 
 function QuizResults(props) {
   const increaseSales = {
-    heading: "Increase your sales here",
+    heading: "Increase your sales",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
     img: "Appointment-.png",
     link_to: "/salesTraining",
   };
   const appDev = {
-    heading: "Build Mobile and Web applications heading",
+    heading: "Build Mobile and Web applications",
     content:
       "Build Mobile and Web applications content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
     img: "Appointment-.png",
     link_to: "/applicationDevelopment",
   };
   const virtualTeam = {
-    heading: "Build Virtual Team heading",
+    heading: "Build Virtual Team",
     content:
       "Build Virtual Team content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
     img: "Appointment-.png",
     link_to: "/businessEvents",
   };
   const marketing = {
-    heading: "Branding and Marketing heading",
+    heading: "Branding and Marketing",
     content:
       "Branding and Marketing content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
     img: "Appointment-.png",
