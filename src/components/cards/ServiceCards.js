bbad9f797a54b5a7a6c499749d9090c1b74e4de5
GURDAYAL SINGH DHILLON <20343529@students.latrobe.edu.au>
2020-11-05 12:56:19 +1100

    about page content update

--- src/components/cards/ServiceCards.js
+++ src/components/cards/ServiceCards.js
@@ -5,10 +5,13 @@ import "./serviceCards.css";
 export default function ServiceCards(props) {
   const data = props.data;
   const imgSrc = require("../../images/" + data.cardImg);
+  const classes = data.classes
+    ? data.classes.join(" ")
+    : "serice-card-container";
   //
   return (
     <Link to={data.link_to}>
-      <div className={data.classes.join(" ")}>
+      <div className={classes}>
         <img src={imgSrc} alt={data.cardHeading}></img>
         <h4>{data.cardHeading}</h4>
         <p>{data.cardDescription}</p>
