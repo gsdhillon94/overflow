4505609b010916d6fbcabc6de83f19c6816820ae
GURDAYAL SINGH DHILLON <20343529@students.latrobe.edu.au>
2020-11-13 12:55:35 +1100

    content update

--- src/components/blog/BlogList.js
+++ src/components/blog/BlogList.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Blog from "./Blog";
 export default function BlogList(props) {
-  const data = props;
+  let data = { ...props };
+  const [selectedCategory, setselectedCategory] = useState("All");
+
+  console.log(data, "check");
   const blogs =
     data.data &&
     data.data.map((item, i) => {
-      return <Blog key={item + i} data={item}></Blog>;
+      if (selectedCategory === "All") {
+        return <Blog key={item + i} data={item}></Blog>;
+      } else {
+        if (item.bCat === selectedCategory) {
+          return <Blog key={item + i} data={item}></Blog>;
+        }
+      }
     });
   const distinct = (val, index, self) => {
     return self.indexOf(val) === index;
@@ -15,13 +24,27 @@ export default function BlogList(props) {
     return item.bCat;
   });
   categories = categories.filter(distinct);
-  console.log(categories);
+  categories.unshift("All");
 
+  const showSelectedCategory = (cat) => {
+    setselectedCategory(cat);
+  };
+  useEffect(() => {
+    console.log(data, "data after re render");
+  }, [selectedCategory]);
   return (
     <>
       <div className="section no-height categories">
-        {categories.map((item) => {
-          return <button>{item}</button>;
+        {categories.map((item, index) => {
+          return (
+            <button
+              className={selectedCategory === item ? "cat-active" : null}
+              key={item + index}
+              onClick={() => showSelectedCategory(item)}
+            >
+              {item}
+            </button>
+          );
         })}
       </div>
       <div className="section wrap-content space-evenly">{blogs}</div>
