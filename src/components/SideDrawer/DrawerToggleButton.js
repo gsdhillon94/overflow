b4df7a7890afc25b734742e846ebcc5446dcc2df
GURDAYAL SINGH DHILLON <20343529@students.latrobe.edu.au>
2020-11-13 17:04:16 +1100

    content update

--- src/components/SideDrawer/DrawerToggleButton.js
+++ src/components/SideDrawer/DrawerToggleButton.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./DrawerToggleButton.css";
 const DrawerToggleButton = (props) => (
   <button className="toggle-button" onClick={props.click}>
+    <i className="fas fa-bars"></i>
+    {/* <div className="toggle-button__line"></div>
     <div className="toggle-button__line"></div>
-    <div className="toggle-button__line"></div>
-    <div className="toggle-button__line"></div>
+    <div className="toggle-button__line"></div> */}
   </button>
 );
 
